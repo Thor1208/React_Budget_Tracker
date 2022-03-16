f69df06e3c522256a248f1c1c9fd71240ab06b92
Thor1208 <97014168+Thor1208@users.noreply.github.com>
2022-03-16 18:07:45 +0530

    Tutorials Done...Challenges Remaining

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from "./components/Budget";
 import Remaining from "./components/Remaining";
 import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
+import { AppProvider } from "./context/AppContext";
 
 const App = () => {
-  return(
-    <div className='container'>
-      <h1 className="mt-3">Budget Planner</h1>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <Budget></Budget>
+  return (
+    <AppProvider>
+      <div className='container'>
+        <h1 className="mt-3">Budget Planner</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget></Budget>
+          </div>
+          <div className="col-sm">
+            <Remaining></Remaining>
+          </div>
+          <div className="col-sm">
+            <ExpenseTotal></ExpenseTotal>
+          </div>
         </div>
-        <div className="col-sm">
-          <Remaining></Remaining>
+        <h3 className="mt-3">Expenses</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseList></ExpenseList>
+          </div>
         </div>
-        <div className="col-sm">
-          <ExpenseTotal></ExpenseTotal>
+        <h3 className="mt-3">Add Expense</h3>
+        <div className="mt-3">
+          <div className="col-sm">
+            <AddExpenseForm></AddExpenseForm>
+          </div>
         </div>
       </div>
-    </div>
+    </AppProvider>
+
   );
 }
 
